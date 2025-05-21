67558fd48caaebf102978e3b15366fb4c886be5c
Daniel Frutos <daniel.frutos@xcaliburmp.com>
2025-05-21 17:01:48 +0200

    Up to 219

---
+++ routing-01-starting-setup/src/pages/UsersList.vue
@@ -0,0 +1,53 @@
+<template>
+  <button @click="confirmInput">Confirm</button>
+  <button @click="saveChanges">Save Changes</button>
+  <ul>
+    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+  </ul>
+</template>
+
+<script>
+import UserItem from '../components/users/UserItem.vue';
+
+export default {
+  components: {
+    UserItem,
+  },
+  inject: ['users'],
+  data(){
+    return {
+      changesSaved: false
+    }
+  },
+  methods: {
+    confirmInput() {
+      this.$router.push('/teams');
+    },
+    saveChanges(){
+      this.changesSaved = true;
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log(to, from);
+    next();
+  },
+  beforeRouteLeave(to, from, next){
+    console.log('UserList beforeRouteLeave' + to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm('Are you sure? Unsaved data');
+      next(userWantsToLeave);
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+</style>
